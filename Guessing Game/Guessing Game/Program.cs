c703d3156fb72b38538ab845b17a8fd605c62144
Brandon24 <brandonreinert24@gmail.com>
2014-03-05 10:05:09 -0500

    Day1

---
+++ Guessing Game/Guessing Game/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int guess;
+            String Input;
+            while (true)
+            {
+                
+               if(guess >= 0 && guess <=100)
+               {
+                guess = 50;
+                {
+                Console.WriteLine("Is your guess {0}? h, l, e ", guess);
+                Input = Console.ReadLine();
+               
+                    if (Input == "h")
+                {
+                    guess = 75;
+                    }
+                }
+                
+                    else(Input = "l");
+                 
+                {
+                    guess = 25;
+                }
+
+
+
+
+                }
+
+            }
+
+        }
+
+    }
+}
+            
+        
+    
+
+
+      
+
+
+
+            
